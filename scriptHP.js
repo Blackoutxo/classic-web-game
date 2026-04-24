4eb85356c9317edd621f144a7c541a48d3be7ae5
Blackout180 <Semperx123@gmail.com>
2026-04-24 23:23:29 +0545

    initial commit added homepage, buttons

---
+++ scriptHP.js
@@ -0,0 +1,10 @@
+const snakeButton = document.getElementById("snake");
+const tetrisButton = document.getElementById("tetris");
+
+const soundFX = new Audio("./assets/");
+
+function snakeClick() {
+    setTimeout(() => {
+        
+    }, 3000);
+}
