e56a826f7a51d15f3756ea80b774b673a876f641
Blackout180 <Semperx123@gmail.com>
2026-05-02 09:11:27 +0545

    Big change incoming

---
+++ snake/snake_script.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById('game-ground');
+const ctx = canvas.getContext('2d');
+
+const boardIMG = document.getElementById('voila');
+
+
+// ---------Canvas--------- //
+
+function startGame() {
+
+}
+
